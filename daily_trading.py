b6e66c7ee8e1f85fda35e52939ed249e1c7d182e
sina <78888004+30-na@users.noreply.github.com>
2021-03-20 01:13:25 -0500

    Add files via upload

--- daily_trading.py
+++ daily_trading.py
@@ -17,10 +17,10 @@ def fidelity_top_stock(file_name):
     return stock_symbol
 
 def download_raw_data(symbol_name):
-    raw_data = yf.download(tickers = symbol_name, period = '180d', interval = '1d' )
+    raw_data = yf.download(tickers = symbol_name, period = '300d', interval = '1d' )
     return raw_data
 
-def add_signal_to(raw_data):
+def add_SMA_to(raw_data):
     
     def SMA(close_price, period = 9):
         sma = close_price.rolling(window=period).mean().iloc[period-1: ]
@@ -31,30 +31,109 @@ def add_signal_to(raw_data):
     raw_data['sma_position'] = ((raw_data['Adj Close'] - raw_data['SMA']) / raw_data['Adj Close']) * 100
     return raw_data
 
-today = datetime.date.today()
-stock_list = fidelity_top_stock(file_name)
+def add_SMA09_to(raw_data):
+    
+    def SMA(close_price, period = 9):
+        sma = close_price.rolling(window=period).mean().iloc[period-1: ]
+        return sma.to_frame()
+    
+    sma = SMA(raw_data['Adj Close'])
+    raw_data['SMA09'] =  sma
+    return raw_data
 
-# for i in stock_list:
-#     df = download_raw_data(i)
-#     df = add_signal_to(df)
-#     print(i,end = " ")
-#     print ( df['sma_position'].tail(1))
-        
-   
+def add_SMA21_to(raw_data):
+    
+    def SMA(close_price, period = 21):
+        sma = close_price.rolling(window=period).mean().iloc[period-1: ]
+        return sma.to_frame()
+    
+    sma = SMA(raw_data['Adj Close'])
+    raw_data['SMA21'] =  sma
+    return raw_data
+
+today = str(datetime.date.today())
+stock_list = fidelity_top_stock(file_name)
+upward_trend_stock_list = []
+
+
+# def str1_cross_SMA (symbol):
+
+
+df = download_raw_data("LOGI")
+df = add_SMA09_to(df)
+df = add_SMA21_to(df)
+df["trend"] = df.iloc[:,6] - df.iloc[: ,7] 
+
+print(df.iloc[-10: , [4,6,7,8]]) 
+
+symbol = "U"
+interval = 250
+def regular_profit(symbol_df, interval):
+    profit = (( symbol_df.iloc[-1 , 4] - symbol_df.iloc[-interval , 4] ) /  symbol_df.iloc[-interval , 4]) * 100
+    return profit
+
+
+def str01_cross_sma(symbol_df, interval):
+    position = "sell"
+    profit = 0
+    count_buy_day = 0
+    count_sell_day = 0
+    for i in range(interval):
+        if position == "sell":
+            count_sell_day += 1
+            
+        if position == "buy":
+            count_buy_day += 1
+        # print(i, end= " ")
+        # print(position, end=' ')
+        # print(symbol_df.iloc[-interval + i , 8], end=" ")
+        # print(count_sell_day, end=" ")
+        # print(count_buy_day)
+         
+        if symbol_df.iloc[-interval + i , 8] > 1 and position == "sell":
+            buy_price = symbol_df.iloc[-interval + i , 4]
+            position = "buy"
+            
+        elif symbol_df.iloc[-interval + i , 8] < -1 and position == "buy":
+            position = 'sell'
+            profit = (symbol_df.iloc[-interval + i , 4] - buy_price) + profit
+    profit = (profit / symbol_df.iloc[-interval , 4]) * 100
+    # print(count_buy_day)
+    # print(count_sell_day)     
+    return profit
+
+print(regular_profit(df, interval))
+print(str01_cross_sma(df, interval))    
+
+
+
+
 # plot 
-symbol = 'DHR'
-df = download_raw_data(symbol)
-df = add_signal_to(df)
+# symbol = 'ENVA'
+# df = download_raw_data(symbol)
+# df = add_signal_to(df)
+# plt.figure(figsize = (24,12))
+# plt.plot(df['2020':'2021']['Adj Close'], label='Adj Close', linewidth = 2)
+# plt.plot(df['2020':'2021']['SMA'], label='9 days rolling SMA', linewidth = 1.5)
+# plt.xlabel('Date')
+# plt.ylabel('Adjusted closing price ($)')
+# plt.title('Simple Moving Average')
+# plt.legend()
+# plt.show()
+
+
+#plot
+symbol = 'LOGI'
+period = 500
 plt.figure(figsize = (24,12))
-plt.plot(df['2020':'2021']['Adj Close'], label='Adj Close', linewidth = 2)
-plt.plot(df['2020':'2021']['SMA'], label='9 days rolling SMA', linewidth = 1.5)
+plt.plot(df.iloc[-period: , [4]], label='Adj Close', linewidth = 2)
+plt.plot(df.iloc[-period: , [6]], label='9 days rolling SMA', linewidth = 1.5)
+plt.plot(df.iloc[-period: , [7]], label='21 days rolling SMA', linewidth = 1.5)
 plt.xlabel('Date')
 plt.ylabel('Adjusted closing price ($)')
 plt.title('Simple Moving Average')
 plt.legend()
 plt.show()
-
-
 
 
 
