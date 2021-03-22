da37d43f9846a571c1077d21bae91ae02f8de99d
sina <78888004+30-na@users.noreply.github.com>
2021-03-22 01:56:51 -0500

    Add files via upload

--- daily_trading.py
+++ daily_trading.py
@@ -8,8 +8,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
 import datetime
+import random
 
 file_name = 'screener_results.xls'
+today = str(datetime.date.today())
+interval = 250
+
+#df["trend"] = df.iloc[:,6] - df.iloc[: ,7] 
+
 def fidelity_top_stock(file_name):
     df = pd.read_excel(file_name)
     last_symbol_index = (pd.isnull(df['Symbol']).to_numpy().nonzero()[0][0])-1
@@ -51,89 +57,135 @@ def add_SMA21_to(raw_data):
     raw_data['SMA21'] =  sma
     return raw_data
 
-today = str(datetime.date.today())
-stock_list = fidelity_top_stock(file_name)
-upward_trend_stock_list = []
+def plot_cross(symbol, period):
+    df = download_raw_data(symbol)
+    df = add_SMA09_to(df)
+    df = add_SMA21_to(df)
+    plt.figure(figsize = (24,12))
+    plt.plot(df.iloc[-period: , [4]], label='Adj Close', linewidth = 2)
+    plt.plot(df.iloc[-period: , [6]], label='9 days rolling SMA', linewidth = 1.5)
+    plt.plot(df.iloc[-period: , [7]], label='21 days rolling SMA', linewidth = 1.5)
+    plt.xlabel('Date')
+    plt.ylabel('Adjusted closing price ($)')
+    plt.title('Simple Moving Average')
+    plt.legend()
+    plt.show()
 
 
-# def str1_cross_SMA (symbol):
+def get_SPY_stock_list():
+    table = pd.read_html("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
+    df = table[0]
+    df.to_csv("S&P500-Symbols.csv", columns=['Symbol'])
+    return df["Symbol"]
 
+def profit(stock_list, interval):
+    sum = 0
+    def regular_profit(symbol_df, interval):
+        # if interval > symbol_df.shape[0]:
+        #     interval = symbol_df.shape[0]
+        profit = (( symbol_df.iloc[-1 , 4] - symbol_df.iloc[-interval , 4] ) /  symbol_df.iloc[-interval , 4]) * 100
+        return profit
 
-df = download_raw_data("LOGI")
-df = add_SMA09_to(df)
-df = add_SMA21_to(df)
-df["trend"] = df.iloc[:,6] - df.iloc[: ,7] 
+    for i in stock_list:
+        i = i.replace('.','-')
+        df = download_raw_data(i)
+        sum = regular_profit(df, interval) + sum
+    return sum/len(stock_list)
 
-print(df.iloc[-10: , [4,6,7,8]]) 
-
-symbol = "U"
-interval = 250
-def regular_profit(symbol_df, interval):
-    profit = (( symbol_df.iloc[-1 , 4] - symbol_df.iloc[-interval , 4] ) /  symbol_df.iloc[-interval , 4]) * 100
-    return profit
-
-
-def str01_cross_sma(symbol_df, interval):
-    position = "sell"
-    profit = 0
-    count_buy_day = 0
-    count_sell_day = 0
-    for i in range(interval):
-        if position == "sell":
-            count_sell_day += 1
-            
-        if position == "buy":
-            count_buy_day += 1
-        # print(i, end= " ")
-        # print(position, end=' ')
-        # print(symbol_df.iloc[-interval + i , 8], end=" ")
-        # print(count_sell_day, end=" ")
+def str1_cross_profit(stock_list, interval):
+    
+    def cross_sma(symbol_df, interval):
+        position = "sell"
+        profit = 0
+        # count_buy_day = 0
+        # count_sell_day = 0
+        for i in range(interval):
+        #     if position == "sell":
+        #         count_sell_day += 1
+                
+        #     if position == "buy":
+        #         count_buy_day += 1
+            # print(i, end= " ")
+            # print(position, end=' ')
+            # print(symbol_df.iloc[-interval + i , 8], end=" ")
+            # print(count_sell_day, end=" ")
+            # print(count_buy_day)
+             
+            if symbol_df.iloc[-interval + i , 8] > 1 and position == "sell":
+                buy_price = symbol_df.iloc[-interval + i , 4]
+                position = "buy"
+                
+            elif symbol_df.iloc[-interval + i , 8] < -1 and position == "buy":
+                position = 'sell'
+                profit = (symbol_df.iloc[-interval + i , 4] - buy_price) + profit
+        profit = (profit / symbol_df.iloc[-interval , 4]) * 100
         # print(count_buy_day)
-         
-        if symbol_df.iloc[-interval + i , 8] > 1 and position == "sell":
-            buy_price = symbol_df.iloc[-interval + i , 4]
-            position = "buy"
+        # print(count_sell_day)     
+        return profit
+   
+    sum = 0
+    for i in stock_list:
+        i = i.replace('.','-')
+        df = download_raw_data(i)       
+        df = add_SMA09_to(df)       
+        df = add_SMA21_to(df)
+        df['trend'] = ((df.iloc[:,6] - df.iloc[:,7])/df.iloc[:,6])*100
+        sum = cross_sma(df, interval) + sum
+    
+    return sum/len(stock_list)
+
+
+budget = 1000
+interval = 30
+stock_list = fidelity_top_stock(file_name)
+my_portfo = dict()
+sell_list = []
+for i in range(interval):
+    #buy
+    while budget >= 100:
+      stock_symbol = random.choice(stock_list)
+      while stock_symbol in my_portfo:
+          stock_symbol = random.choice(stock_list)
+      symbol_df = download_raw_data(stock_symbol)
+      close_price = symbol_df.iloc[-interval + i , 4]
+      # print(type(stock_symbol))
+      # print(close_price)
+      stock_share = 100/close_price
+      budget = budget - 100
+      # print(my_portfo)
+      # print(stock_share)
+      my_portfo[stock_symbol] = stock_share
+  
+    #sell 
+    for j in my_portfo:   
+        symbol_df = download_raw_data(j)
+        close_price = symbol_df.iloc[-interval + i , 4]
+        stock_share = my_portfo[j]
+        stock_value = close_price * stock_share
+        
+        if  stock_value > 105 or stock_value < 95:
+            budget = stock_value + budget
+            sell_list.append(j)
             
-        elif symbol_df.iloc[-interval + i , 8] < -1 and position == "buy":
-            position = 'sell'
-            profit = (symbol_df.iloc[-interval + i , 4] - buy_price) + profit
-    profit = (profit / symbol_df.iloc[-interval , 4]) * 100
-    # print(count_buy_day)
-    # print(count_sell_day)     
-    return profit
+    for j in sell_list:
+        my_portfo.pop(j)
+    
+    sell_list.clear()
+    
+investment_value = budget
+for j in my_portfo:
+    symbol_df = download_raw_data(j)
+    close_price = symbol_df.iloc[-interval + i , 4]
+    stock_share = my_portfo[j]
+    stock_value = close_price * stock_share
+    investment_value = investment_value + stock_value
 
-print(regular_profit(df, interval))
-print(str01_cross_sma(df, interval))    
-
-
-
-
-# plot 
-# symbol = 'ENVA'
-# df = download_raw_data(symbol)
-# df = add_signal_to(df)
-# plt.figure(figsize = (24,12))
-# plt.plot(df['2020':'2021']['Adj Close'], label='Adj Close', linewidth = 2)
-# plt.plot(df['2020':'2021']['SMA'], label='9 days rolling SMA', linewidth = 1.5)
-# plt.xlabel('Date')
-# plt.ylabel('Adjusted closing price ($)')
-# plt.title('Simple Moving Average')
-# plt.legend()
-# plt.show()
+print(investment_value)
 
 
-#plot
-symbol = 'LOGI'
-period = 500
-plt.figure(figsize = (24,12))
-plt.plot(df.iloc[-period: , [4]], label='Adj Close', linewidth = 2)
-plt.plot(df.iloc[-period: , [6]], label='9 days rolling SMA', linewidth = 1.5)
-plt.plot(df.iloc[-period: , [7]], label='21 days rolling SMA', linewidth = 1.5)
-plt.xlabel('Date')
-plt.ylabel('Adjusted closing price ($)')
-plt.title('Simple Moving Average')
-plt.legend()
-plt.show()
+
+SPY_list = list(get_SPY_stock_list())
+SPY_list.remove('VNT')
 
 
 
@@ -141,3 +193,7 @@ plt.show()
 
 
 
+
+
+
+    
